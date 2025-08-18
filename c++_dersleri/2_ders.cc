b954577468a6caccc3869992c4e6113c31dc5ee5
PolluXx571 <alyapegasus@mail.ru>
2025-08-18 17:09:24 +0500

    2 nd day

---
+++ c++_dersleri/2_ders.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "this is second work C++ \n"
+         << endl;
+    return 0;
+}
--- c_dersleri/1_ders.c
+++ c_dersleri/1_ders.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 
-int sum (){
+int sum()
+{
     int t = 3;
     int s = 5;
 
     printf("%d\n", t * s);
 };
-
-
-int main (){
-    sum();
-    printf("hello my name is Suleyman");
-    
-}
-
